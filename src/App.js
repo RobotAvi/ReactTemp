032256a0a1dd100d2205f82dc0526540180d67ac
Avenir Voronov <avoronov@korusconsulting.ru>
2022-04-04 17:13:08 +0300

    Added 2 counter realisations Added simple page for style test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
+import Counter from "./components/Counter";
+import CounterClass from "./components/CounterClass";
 
 function App() {
-    const [value, setValue] = useState("Template");
 
 
     return (
         <div className="App">
-            <h1>Надпись</h1>
-            <h1>{value}</h1>
-            <input
-                type="text"
-                value={value}
-                onChange={event => setValue((event.target.value))}
-            />
+            <div className="post">
+                <div className="post__content">
+                    <strong>Надпись</strong>
+                    <div>你好</div>
+                </div>
+                <div className="post__buttons">
+                    <button>Remove</button>
+                </div>
+            </div>
+            <Counter/>
+            <CounterClass/>
 
         </div>
     );
