48ecb56007ac6e96b1fbc155a6c437dd132ce68a
Avenir Voronov <avoronov@korusconsulting.ru>
2022-04-04 16:47:15 +0300

    Added input and caption Added components directory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [value, setValue] = useState("Template");
+
+
+    return (
+        <div className="App">
+            <h1>Надпись</h1>
+            <h1>{value}</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={event => setValue((event.target.value))}
+            />
+
+        </div>
+    );
 }
 
 export default App;
